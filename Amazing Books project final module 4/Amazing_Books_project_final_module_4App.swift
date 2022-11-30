559c1868e6217915d56f1b88e6e636e28886b1f4
abbbringu <brianphi.nguyen@gmail.com>
2022-11-30 17:33:48 +0100

    Initial Commit

---
+++ Amazing Books project final module 4/Amazing_Books_project_final_module_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Amazing_Books_project_final_module_4App.swift
+//  Amazing Books project final module 4
+//
+//  Created by Brian Nguyen on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct Amazing_Books_project_final_module_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
